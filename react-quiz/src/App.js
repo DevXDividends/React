ea583c508adb567eeabc37101dde6544ee10ee93
Aditya Dengale <adityadengale16@gmail.com>
2026-07-20 21:20:44 +0530

    add code to react-quiz app

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,6 +1,48 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+const initial_state = {
+  questions: [],
+  // 'loading' 'error' 'ready' 'active' 'finished'  
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      }
+    default:
+      throw new Error("Action Unknown");
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      }
+  }
+
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initial_state);
+
+  useEffect(function () {
+    async function loadData() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        if (!response.ok)
+          throw new Error("Failed to load data ");
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data })
+      }
+      catch (err) {
+        dispatch({ type: "dataFailed", payload: "Erroe while receiving data" })
+      }
+    }
+    loadData();
+  }, [])
+
   return (
     <div className="app">
       <Header />
