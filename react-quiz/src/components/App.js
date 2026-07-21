68e701a4a639281e7fa197d924ee7f502ec67a01
Aditya Dengale <adityadengale16@gmail.com>
2026-07-21 13:39:04 +0530

    add more code to react-quiz

---
+++ react-quiz/src/components/App.js
@@ -0,0 +1,127 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+
+const initial_state = {
+  questions: [],
+  // 'loading' 'error' 'ready' 'active' 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("Action Unknown");
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "newAnswer":
+      const quesiton = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === quesiton.correctOption
+            ? state.points + quesiton.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+      };
+  }
+}
+export default function App() {
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initial_state,
+  );
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0,
+  );
+
+  useEffect(function () {
+    async function loadData() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        if (!response.ok) throw new Error("Failed to load data ");
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed", payload: "Erroe while receiving data" });
+      }
+    }
+    loadData();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main className="main">
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
+        )}
+      </Main>
+    </div>
+  );
+}
---
+++ react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,18 @@
+function FinishScreen({ points, maxPossiblePoints }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙂";
+  if (percentage >= 0 && percentage < 50) emoji = "🙄";
+  if (percentage === 0) emoji = "🗿";
+  if (percentage)
+    return (
+      <p className="result">
+        <span>{emoji}</span> You Scored : <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+    );
+}
+
+export default FinishScreen;
